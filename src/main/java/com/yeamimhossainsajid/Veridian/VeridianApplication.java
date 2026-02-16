dd7177dea2ed590153402724a9996fbaca056cec
Sajid <yeamimhossainsajid@gmail.com>
2026-02-16 15:49:43 +0600

    first commit

---
+++ src/main/java/com/yeamimhossainsajid/Veridian/VeridianApplication.java
@@ -0,0 +1,13 @@
+package com.yeamimhossainsajid.Veridian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeridianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeridianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeamimhossainsajid/Veridian/VeridianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeamimhossainsajid.Veridian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeridianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
